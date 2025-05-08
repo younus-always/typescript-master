78a924c784580a1f2eea4c1edab649c5c5188084
younus-always <younusofficial443@gmail.com>
2025-05-08 09:23:38 +0600

    class constructor problem solve

--- index.ts
+++ index.ts
@@ -6,9 +6,50 @@ function formatString(input: string, toUpper?: boolean) {
             const lowerText = input.toLowerCase()
             return lowerText
       }
+};
+formatString("Hello")
+formatString("Hello", true)
+formatString("Hello", false)
+
+
+
+function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
+      const rating = items.filter(item => item.rating >= 4)
+      return rating
+}
+const books = [
+      { title: "Book A", rating: 4.5 },
+      { title: "Book B", rating: 3.2 },
+      { title: "Book C", rating: 5.0 }
+];
+filterByRating(books)
+
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+      const concateArray = arrays.flat()
+      return concateArray
+};
+concatenateArrays(["a", "b"], ["c"])
+concatenateArrays([1, 2], [3, 4], [5])
+
+
+class Vehicle {
+      constructor(private make: string, private year: number) { }
+      getInfo() {
+            return `Make: ${this.make}, Year: ${this.year}`
+      }
 }
 
-const result = formatString("Hello")    
-const result2 = formatString("Hello", true)   
-const result3 = formatString("Hello", false)
+class Car extends Vehicle {
+      constructor(make: string, year: number, public model: string) {
+            super(make, year)
+      }
+      getModel() {
+            return `Model: ${this.model}`
+      }
+};
+const myCar = new Car("Toyota", 2020, "Corolla");
+myCar.getInfo();  
+myCar.getModel();  
+
 
